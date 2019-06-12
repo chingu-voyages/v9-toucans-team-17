9809b83004267f7d59c8094e7a103ddaee28df34
Yonas15 <coding.yonas@gmail.com>
2019-06-12 17:34:12 +0700

    create the responsive part of our landing page

--- js/main.js
+++ js/main.js
@@ -2,5 +2,15 @@ window.onload = load;
 
 function load() {
   TweenMax.to(".content-header", 2, {opacity: 1, scale: 1, ease: Circ.easeOut});
-  TweenMax.to(".li-down", 1.3, {y: 0, delay: 1, ease: Circ.easeOut});
+  TweenMax.from(".li-down", 1.3, {y: -60, delay: 1, ease: Circ.easeOut});
 }
+
+// algorithm for mobile navbar
+let hamburgerButton = document.getElementById('hamburger-icon');
+
+hamburgerButton.addEventListener('click', () => {
+  document.querySelector('.bar1').classList.toggle('change');
+  document.querySelector('.bar2').classList.toggle('change');
+  document.querySelector('.bar3').classList.toggle('change');
+  document.querySelector('#nav ul').classList.toggle('active');
+})
