b38287537ca07702f41120353da7dee1f60b11ce
Tri-Waliangga <Waliangga@Gmail.com>
2019-06-13 02:58:08 +0700

    add animation to Our Product

--- js/main.js
+++ js/main.js
@@ -14,3 +14,31 @@ hamburgerButton.addEventListener('click', () => {
   document.querySelector('.bar3').classList.toggle('change');
   document.querySelector('#nav ul').classList.toggle('active');
 })
+
+// scroll event triggered by ID
+const blackRice = document.getElementById('black-rice');
+const redRice = document.getElementById('red-rice');
+const brownRice = document.getElementById('brown-rice');
+
+  let yBlack = blackRice.scrollHeight;
+  let yRed = redRice.scrollHeight;
+  let yBrown = brownRice.scrollHeight;
+function scroll() {
+  if(document.body.scrollTop > yBlack + 100 || document.documentElement.scrollTop > yBlack + 100) {
+    console.log(yRed + "px Bred");
+    TweenMax.to("#bg-black-rice", 1, {x: 0, ease: Circ.easeOut});
+    TweenMax.to(".bg-text-black", 1, {x: 50, delay: 1, ease: Circ.easeOut});
+  }
+  if(document.body.scrollTop > yRed + 500 || document.documentElement.scrollTop > yRed + 500) {
+    console.log(yRed + "px Yred");
+    TweenMax.to("#bg-red-rice", 1, {x: 0, ease: Circ.easeOut});
+    TweenMax.to(".bg-text-red", 1, {x: -25, delay: 1, ease: Circ.easeOut});
+  }
+  if(document.body.scrollTop > yBrown + 1000 || document.documentElement.scrollTop > yBrown + 1000) {
+    TweenMax.to("#bg-brown-rice", 1, {x: 0, ease: Circ.easeOut});
+    TweenMax.to(".bg-text-brown", 1, {x: 50, delay: 1, ease: Circ.easeOut});
+  }
+}
+
+window.onscroll = function() {scroll()};
+  
